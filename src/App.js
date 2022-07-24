c61226f3ba67e9a5d53cc5771b6d234e84859db9
Milos <djoledj@hotmail.com>
2022-07-25 01:16:59 +0200

    final update

--- src/App.js
+++ src/App.js
@@ -1,27 +1,32 @@
-import React from 'react';
+import React from "react";
 
-import { Footer, Blog, Possibility, Features, WhatGPT3, Header} from './containers';
-import { CTA, Brand, Navbar} from './components';
-import './App.css';
-
+import {
+  Footer,
+  Blog,
+  Possibility,
+  Features,
+  WhatGPT3,
+  Header,
+} from "./containers";
+import { CTA, Brand, Navbar } from "./components";
+import "./App.css";
 
 const App = () => {
-    return (
-        <div className='App'>
-             <div className='gradient__bg'>
-                 <Navbar />
-                 <Header />
-             </div>
-             <Brand />
-             <WhatGPT3 />
-             <Features />
-             <Possibility />
-             <CTA />
-             <Blog />
-             <Footer />
+  return (
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
+    </div>
+  );
+};
 
-        </div>
-    )
-}
-
-export default App
+export default App;
